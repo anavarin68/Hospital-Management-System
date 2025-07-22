fbbc8b4b3449efb14fb36b5ceca569ee42dfe0c7
Nitish <kumarpateln7@gmail.com>
2025-07-23 04:19:48 +0530

    Added Update to Appointment Status

--- src/main/java/com/bbd/HospitalManagement/Controller/AppointmentController.java
+++ src/main/java/com/bbd/HospitalManagement/Controller/AppointmentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,4 +42,17 @@ public class AppointmentController {
 		appointmentService.deleteAppointment(id);
 		return "Appointment deleted with id:" + id;
 	}
+
+	@PutMapping("/update/{id}")
+	public AppointmentDetails markAppointmentStatus(@PathVariable Long id) {
+		AppointmentDetails appointment = appointmentService.getAppointmentById(id);
+		if (appointment != null) {
+			appointment.setStatus("Completed");
+			return appointmentService.saveAppointment(appointment);
+		} else {
+			throw new RuntimeException("Appointment not found with id: " + id);
+		}
+
+	}
+
 }
