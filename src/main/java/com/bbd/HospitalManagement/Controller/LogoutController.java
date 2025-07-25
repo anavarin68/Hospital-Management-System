7afded2d13874f872e54fc1b4298325b8f0cbc5e
Nitish <kumarpateln7@gmail.com>
2025-07-26 03:32:02 +0530

    Added CSS

--- src/main/java/com/bbd/HospitalManagement/Controller/LogoutController.java
+++ src/main/java/com/bbd/HospitalManagement/Controller/LogoutController.java
@@ -2,6 +2,7 @@ package com.bbd.HospitalManagement.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -9,8 +10,16 @@ import jakarta.servlet.http.HttpSession;
 public class LogoutController {
 
 	@GetMapping("/logout")
-    public String logout(HttpSession session) {
-        session.invalidate(); // Invalidate the session
-        return "redirect:/";  // Redirect to home page
-    }
+	public String logout(HttpSession session, RedirectAttributes redirectAttributes) {
+		String redirectUrl = "/";
+
+		if (session.getAttribute("doctorId") != null) {
+			redirectUrl = "/doctor/login";
+		} else if (session.getAttribute("patientId") != null) {
+			redirectUrl = "/patient/login";
+		}
+		session.invalidate();
+		redirectAttributes.addFlashAttribute("logoutSuccess", "Logout Successful!!");
+		return "redirect:" + redirectUrl;
+	}
 }
